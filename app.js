296914199abe0ed3ad04fc8e7e3d30de270aea8a
José María Martín Muñoz <josemaria.martin169@gmail.com>
2022-03-06 20:58:13 +0100

    Test

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 (function(){
+    var mainContainer = document.getElementById("main-container");
+    var qrContainer = document.getElementById("qr-container");
+
+    var height = mainContainer.style.height;
+    document.getElementById("output").innerHTML = getComputedStyle(mainContainer).height + ", " + visualViewport.height + "px";
+
     var input = document.getElementById("input-area");
     var preview = document.getElementById("qr-preview");
     var previewBorder = document.getElementById("qr-preview__border");
+    
 
     var inputStyle = getComputedStyle(input);
     var previewStyle = getComputedStyle(preview);
