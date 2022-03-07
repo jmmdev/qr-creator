938f335c47bc423491520f76fed65e121374c5f4
José María Martín Muñoz <josemaria.martin169@gmail.com>
2022-03-07 12:05:05 +0100

    Some fixes

--- app.js
+++ app.js
@@ -10,11 +10,43 @@
     var input = document.getElementById("input-area");
     var preview = document.getElementById("qr-preview");
     var previewBorder = document.getElementById("qr-preview__border");
-    
+    var previewHeader = document.getElementById("qr-preview__header");
+    var previewFooter = document.getElementById("qr-preview__footer");
 
     var inputStyle = getComputedStyle(input);
     var previewStyle = getComputedStyle(preview);
 
     input.style.height = inputStyle.width;
     previewBorder.style.height = previewStyle.width;
+
+    var previewBorderHeight = parseInt(previewBorder.style.height,10);
+
+    previewHeader.style.height = (previewBorderHeight * 0.1 / 0.8) + "px";
+    previewFooter.style.height = (previewBorderHeight * 0.1 / 0.8) + "px";
+
+    var previewTotalHeight = previewBorderHeight + parseInt(previewHeader.style.height,10) * 2;
+
+    var options = document.getElementById("qr-options");
+    var buttons = document.getElementById("save-buttons");
+
+    options.style.height = (window.innerHeight - previewTotalHeight) * 0.80 + "px";
+    buttons.style.height = (window.innerHeight - previewTotalHeight) * 0.20 + "px";
 })();
+
+function showOptions(){
+    var options = document.getElementById("qr-container");
+    var dropdown = document.getElementById("dropdown");
+
+    var optionsStyle = getComputedStyle(options);
+    var dropdownStyle = getComputedStyle(dropdown);
+
+    if(optionsStyle.display === "none"){
+        options.style.display = "block";
+        dropdown.style.boxShadow = "0 1px 0 0 #009c6d";
+    }
+    else if (optionsStyle.display === "block"){
+        options.style.display = "none";
+        dropdown.style.boxShadow = "none";
+    }
+        window.scrollBy(0, window.innerHeight);
+}
